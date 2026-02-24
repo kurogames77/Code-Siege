7551a2a1115ac5aab5c80fc58d6b72d6224641a2
CodeSeige <kurogames77@gmail.com>
2026-02-24 22:23:01 +0800

    Fix ReferenceError: AnimatePresence is not defined in GameNavbar.jsx

--- src/components/game/GameNavbar.jsx
+++ src/components/game/GameNavbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useToast } from '../../contexts/ToastContext';
 import heroesIcon from '../../assets/heroes.png';
